c6360569458ce28f233bb76101b12e4e687d304c
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-02-03 17:38:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves)  { 
+  var temp = "";   
+  for (var i=1; i <= dwarves.length; i++){  
+   temp = temp+i+". "+ dwarves[i-1]+" " ;
+    
+  }
+  return temp;
+  
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+    var temp = [];
+    for(var i = 0; i < planeteerCalls.length; i++){
+      temp.push( planeteerCalls[i].toUpperCase() + "!");
+    }
+
+    return temp;
+   
 }
 
 function longPlaneteerCalls(words) {
+  var check =  false;
+  for(var i=0; i<words.length; i++) {
+    if(words[i].length>4){
+      check = true;
+    }
+  }
+  
+  return check;
 }
 
 function findTheCheese (foods) {
+  
+  for(var i = 0; i<foods.length; i++){
+    if (foods[i]=="cheddar"){
+      return foods[i];
+      }
+      if (foods[i]=="gouda"){
+        return foods[i];
+      }
+      if (foods[i]=="camembert") {
+        return foods[i];
+      }
+      
+  }
+  return "no cheese!";
+  
 }
